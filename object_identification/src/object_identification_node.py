881fddaa863bf130f2b2b932c38aac7c293cd77e
ras <thomas.jantos@rwth-aachen.de>
2018-11-19 17:57:09 +0100

    Initial untested commit for the object manager

--- object_identification/src/object_identification_node.py
+++ object_identification/src/object_identification_node.py
@@ -11,6 +11,7 @@ from keras.preprocessing import image
 from keras.models import load_model
 import tensorflow as tf
 from object_detection.msg import Objects
+import math
 
 #Node to capture images from the camera and save them to the computer.
 
@@ -30,11 +31,11 @@ class ObjectIdentificationNode:
         #                    3:['pruple_cross','I see a purple cross'], 4:['red_ball','I see a red_ball'], 5:['yellow_cube','I see a yellow cube'], 6:['an_object','I see an object']}
 
         self.sound_msg = String()
-        self.sound_msg.data = self.result_msgs[6][1]
+        self.sound_msg.data = self.result_msgs[14][1]
         self.sound_pub = rospy.Publisher('/espeak/string',String,queue_size = 1)
         self.evidence_msg = RAS_Evidence()
         self.evidence_msg.group_number = 6
-        self.evidence_msg.object_id = self.result_msgs[6][0]
+        self.evidence_msg.object_id = self.result_msgs[14][0]
         self.transform_msg = TransformStamped()
         self.evidence_msg.object_location = self.transform_msg
         self.evidence_pub = rospy.Publisher('/evidence',RAS_Evidence,queue_size = 1)
@@ -47,11 +48,114 @@ class ObjectIdentificationNode:
         self.model.summary()
         self.graph = tf.get_default_graph()
 
+        # Structure: [ID, class_ID, pos_x,pox_y, votes]
         self.object_list = []
 
+    #Identification result: integer describing the class => self.result_msgs
+    #Position of the detected object: [x,y] in the map frame
     def manage_objects(self, identification, position):
+        # Check if class was determined at all
         known_object = False
-        return known_object
+        id = None
+        object_counter = len(self.object_list)
+        close_objects = [] # Containing [ID, class_id, distance , votes]
+        # Was an object seen before?
+        if not self.object_list:
+            #Case: no object has been seen before
+            # Add the object depending on the class it was classified as
+            # Known -> 10 votes , unknown -> 3 votes
+            if identification != 14:
+                self.object_list.append([object_counter, identification, position[0], position[1]],10)
+            else:
+                self.object_list.append([object_counter, identification, position[0], position[1]],3)
+
+        else:
+            #Iterate over all objects
+            for obj in self.object_list:
+                #Determine objects close in radius < 20 cm
+                distance = math.sqrt(pow((obj[2]-position[0]), 2) + pow((obj[3] - position[1]),2))
+                if distance <= 0.2:
+                    close_objects.append([obj, distance])
+
+                # Check if there are close by objects
+                if close_objects:
+                    #Sort the objects by distance: closest first
+                    close_objects.sort(key: lambda x: x[1])
+
+                    for object_dist in close_objects:
+                        obj = object_dist[0]
+                        distance = object_dist[1]
+                        idx = self.object_list[obj]
+                        #Check if new object was classified at all
+                        if identification != 14:
+                            if obj[1] == identification:
+                                #Case: A close object has the same class as the detected (and identified object) --> update the object position and increase the votes --> break the for loop
+                                # Therefore the first time an close object with the same class was encountered the function breaks and returns
+                                self.object_list[idx][2] = (self.object_list[idx][2] + position[0]) / 2 #Average x position
+                                self.object_list[idx][3] = (self.object_list[idx][3] + position[1]) / 2 #Average y position
+                                self.object_list[idx][4] += 1 #Increase the votes the object has
+                                known_object = True
+                                id = self.object_list[idx][0]
+                                break
+                            elif obj[1] == 14:
+                                # Case: Detected object was identified but the close objects have class: an_object (== unknown class)
+                                # Update the position but reduce the votes to change the class at one point
+                                # In the case the votes for the object reaches 0, set the class to the one of the identified object and the votes to 3
+                                self.object_list[idx][2] = (self.object_list[idx][2] + position[0]) / 2 #Average x position
+                                self.object_list[idx][3] = (self.object_list[idx][3] + position[1]) / 2 #Average y position
+                                self.object_list[idx][4] -= 1 #Decrease the votes the object has
+                                if self.object_list[idx][4] == 0:
+                                    self.object_list[idx][1] = identification
+                                    self.object_list[idx][4] = 3
+                                continue
+
+                            else:
+                                # Case: An object was detected and identified in close distance to an other object that has a specific class
+                                # Do not update the position, but reduce the votes, because the estimate of class might be wrong
+                                # If votes reaches 0 set the class to: an_object and the votes to 3
+                                self.object_list[idx][4] -= 1 #Decrease the votes the object has
+                                # Set Identification to "an_object"
+                                if self.object_list[idx][4] == 0:
+                                    self.object_list[idx][1] = 14
+                                    self.object_list[idx][4] = 3
+                                continue
+                        else:
+                            #Check if closest object is classified
+                            if obj[1] == 14:
+                                #Case: detected object was not identified and the object is not classified as well
+                                # Update the position and increase the votes for the estimated class (unknown class)
+                                # When the first object was detected this way break and return from the function
+                                self.object_list[idx][2] = (self.object_list[idx][2] + position[0]) / 2 #Average x position
+                                self.object_list[idx][3] = (self.object_list[idx][3] + position[1]) / 2 #Average y position
+                                self.object_list[idx][4] += 1 #Increase the votes the object has
+                                known_object = True
+                                id = self.object_list[idx][0]
+                                break
+                            else:
+                                #Case: The detected object was not identified, but the object in the list was identified
+                                #Update the position, because it still may be the same class, but decrease the number of votes, because the obejct might be missclassified
+                                # If the votes reach 0 set the class to an_obejct and the number of votes to 3
+                                self.object_list[idx][2] = (self.object_list[idx][2] + position[0]) / 2 #Average x position
+                                self.object_list[idx][3] = (self.object_list[idx][3] + position[1]) / 2 #Average y position
+                                self.object_list[idx][4] -= 1 #Decrease the votes the object has
+                                if self.object_list[idx][4] == 0:
+                                    self.object_list[idx][1] = 14
+                                    self.object_list[idx][4] = 3
+                                known_object = True
+                                id = self.object_list[idx][0]
+                                break
+
+                else:
+                    #There is no object close to the newly detected objectself.
+                    # Add object to the list depending on the classification
+                    # Known -> 10 votes , unknown -> 3 votes
+                    if identification != 14:
+                        self.object_list.append([object_counter, identification, position[0], position[1]],5)
+                    else:
+                        self.object_list.append([object_counter, identification, position[0], position[1]],1)
+
+
+        return known_object, id
 
     def evaluate_image(self,cv_image):
         #TODO: Load the trained neural network model and set according to the result the right values for the evidence message and sound message
@@ -94,7 +198,8 @@ class ObjectIdentificationNode:
 
                 if result is not None:
                     rospy.loginfo('Class: '+str(result)+'('+self.result_msgs[result][0]+')')
-                    known_object = self.manage_objects(result,[self.transform_msg.transform.translation.x, self.transform_msg.transform.translation.y])
+                    id = None
+                    known_object, id = self.manage_objects(result,[self.transform_msg.transform.translation.x, self.transform_msg.transform.translation.y])
                     if not known_object:
                         # Set the output string for the sound message and publish it.
                         self.sound_msg.data = self.result_msgs[result][1]
@@ -104,6 +209,8 @@ class ObjectIdentificationNode:
                         self.evidence_msg.object_id = self.result_msgs[result][0]
                         self.evidence_msg.object_location = self.transform_msg
                         self.evidence_pub.publish(self.evidence_msg)
+                    else:
+                        rospy.loginfo("Previously seen Object with ID: " + str(id) + "has been seen again.")
         else:
             self.frame_skipper += 1
 
